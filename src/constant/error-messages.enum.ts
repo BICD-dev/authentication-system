b91c96fb1204351f5f57771177a439a8d235da18
Bright Ihegawam <bright06iheagwam@gmail.com>
2026-04-06 15:11:36 +0100

    Created initial enums for application

---
+++ src/constant/error-messages.enum.ts
@@ -0,0 +1,11 @@
+export enum ErrorMessages{
+    USER_ALREADY_EXISTS="A user with this email already exists",
+    INCORRECT_CREDENTIALS="Incorrect credentials provided",
+    UNAUTHORIZED="Unauthorized access",
+    FORBIDDEN="Forbidden: you do not have access to this resource",
+    INTERNAL_SERVER_ERROR="Internal Server Error",
+    BAD_REQUEST="Bad Request. Please check your input and try again",
+    MISSING_FIELDS="Missing fields",
+
+
+}
---
+++ src/constant/success-messages.enum.ts
@@ -0,0 +1,9 @@
+export enum SuccessMessages {
+    REGISTRATION_INITIATED="Registration Initiated successfully",
+    REGISTRATION_SUCCESSFUL="Registration successful",
+
+    LOGIN_SUCCESSFUL="Login successful",
+    LOGOUT_SUCCESSFUL="Logout successful",
+
+    
+}
