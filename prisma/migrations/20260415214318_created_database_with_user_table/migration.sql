aa0c0db8f7d13ac2c8f6c9559e7cfe0d3eb101dc
Bright Ihegawam <bright06iheagwam@gmail.com>
2026-04-15 23:04:49 +0100

    feat: add Prisma configuration and user model with migrations

---
+++ prisma/migrations/20260415214318_created_database_with_user_table/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "email" TEXT,
+    "phone_number" TEXT,
+    "password" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_phone_number_key" ON "User"("phone_number");
